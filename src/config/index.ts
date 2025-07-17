e7b82dcbcd97bb4daac95d39f3dd96ed37f46af8
tomas-oliveira03 <tumabarbosa@gmail.com>
2025-07-17 22:04:05 +0100

    initial setup

---
+++ src/config/index.ts
@@ -0,0 +1,100 @@
+import 'dotenv/config';
+
+export enum EnvName {
+	DEVELOPMENT = 'development',
+	STAGING = 'staging',
+	PRODUCTION = 'production',
+	LOCAL = 'local',
+	TEST = 'test',
+}
+
+class Envs {
+	private static instance: Envs;
+	private missingKeys: string[] = [];
+
+	public readonly DOCKER_BUILD: boolean = this.getBool('DOCKER_BUILD', false);
+	public readonly PORT: number = this.getInt('PORT', 3001);
+	public readonly DATABASE_URL: string = this.getString(
+		'DATABASE_URL',
+		'postgres://album-tracker-service:album-tracker-service@localhost:5201/album-tracker-service'
+	);
+	public readonly ENCRYPTION_SECRET_IV: string = this.getString('ENCRYPTION_SECRET_IV', 'your_secret');
+	public readonly ENCRYPTION_SECRET_KEY: string = this.getString('ENCRYPTION_SECRET_KEY', 'your_secret');
+	public readonly FRONTEND_URL: string = this.getString('FRONTEND_URL', 'http://localhost:5173');
+	public readonly NODE_ENV: string = this.getString('NODE_ENV', EnvName.LOCAL);
+	public readonly SERVICE: string = this.getString('SERVICE', 'worker');
+
+	private constructor() {
+		if (this.NODE_ENV === EnvName.LOCAL) {
+			return
+		}
+		this.missingKeys = [];
+		this.validateConfig();
+	}
+
+	private validateConfig(): void {
+		if (this.missingKeys.length > 0) {
+			throw new Error(`Missing environment variables: ${this.missingKeys.join(', ')}`);
+		}
+	}
+
+	public static getInstance(): Envs {
+		if (!Envs.instance) {
+			Envs.instance = new Envs();
+		}
+		return Envs.instance;
+	}
+
+	// Computed properties for environment checks
+	public get isServer(): boolean {
+		return this.SERVICE === 'server';
+	}
+
+	public get isWorker(): boolean {
+		return this.SERVICE === 'worker';
+	}
+
+	public get isLocal(): boolean {
+		return this.NODE_ENV === EnvName.LOCAL;
+	}
+
+	public get isDev(): boolean {
+		return this.NODE_ENV === EnvName.DEVELOPMENT;
+	}
+
+	public get isProd(): boolean {
+		return this.NODE_ENV === EnvName.PRODUCTION;
+	}
+
+	private checkFallback(key: string) {
+		if (!this.isLocal) {
+			const val = process.env[key];
+			if (!val) {
+				this.missingKeys.push(key)
+			}
+		}
+	}
+	private getString(key: string, fallback: string): string {
+		this.checkFallback(key)
+		return process.env[key] || fallback
+	}
+
+	private getInt(key: string, fallback: number): number {
+		this.checkFallback(key)
+		const val = process.env[key];
+		if (val === undefined) return fallback;
+		const parsed = parseInt(val, 10);
+		return isNaN(parsed) ? fallback : parsed;
+	}
+
+	private getBool(key: string, fallback: boolean): boolean {
+		this.checkFallback(key)
+		const val = process.env[key];
+		if (val === undefined) return fallback;
+
+		return val.toLowerCase() === "true";
+	}
+
+}
+
+export const envs = Envs.getInstance();
---
+++ src/db/data-source.ts
@@ -0,0 +1,34 @@
+import { envs } from "@/config";
+import "reflect-metadata"
+import { DataSource, DataSourceOptions } from "typeorm";
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+// let SSL_OPTIONS: boolean | {
+// 	rejectUnauthorized: boolean;
+// } = false;
+
+// if (env.NODE_ENV !== EnvName.DEVELOPMENT && env.NODE_ENV !== EnvName.LOCAL) {
+// 	SSL_OPTIONS = { rejectUnauthorized: false };
+// }
+
+const entitiesPaths = envs.DOCKER_BUILD ? 'dist/db/entities/*.js' : 'src/db/entities/*.ts';
+const migrationsPaths = envs.DOCKER_BUILD ? 'dist/db/migrations/*.js' : 'src/db/migrations/*.ts';
+
+const baseSettings: DataSourceOptions = {
+	type: 'postgres',
+	url: envs.DATABASE_URL,
+	ssl: false,
+	// logging: isDev,
+	logging: false,
+	entities: [entitiesPaths],
+	migrations: [migrationsPaths],
+	subscribers: [],
+	migrationsRun: envs.isDev,
+	maxQueryExecutionTime: 5 * 1000,
+	poolSize: 8,
+	logger: 'simple-console',
+	namingStrategy: new SnakeNamingStrategy(),
+};
+
+const AppDataSource = new DataSource(baseSettings);
+export default AppDataSource;
